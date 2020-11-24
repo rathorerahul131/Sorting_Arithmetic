c2eb4bde3a13358c9535af7642187e4149556f6c
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 12:59:06 +0530

    Refactored: User Inputs Taken

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -1,3 +1,15 @@
+  
 #!/bin/bash
 
 echo "Welcome to the sorting Arithmetic Computation Program"
+
+echo "Enter the value of a "
+read a
+
+echo "Enter the value of b "
+read b
+
+echo "Enter the value of c "
+read c
+
+echo "The values of a,b,c are $a, $b, $c"
