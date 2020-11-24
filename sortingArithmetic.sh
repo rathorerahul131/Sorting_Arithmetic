7eb5504daa286326b390ad686b59008c000db9f7
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 13:20:28 +0530

    Refactored: Sorted the Results in Ascending Order

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -28,9 +28,9 @@ echo "The output for the expression a%b+c is $result4"
 # Created a dictionary named All_Results to store all the results
 declare -A All_Results
 
-for ((i=1;i<5;i++))
+for ((i=0;i<4;i++))
 do
-	All_Results["result"$i]=$((result$i))
+	All_Results["result"$i]=$((result$((i+1))))
 done
 
 echo All the results are: ${All_Results[@]}
@@ -43,10 +43,54 @@ sizeOfAll_Results=${#All_Results[@]}
 # Created an array name Result_Array to read from Dictionary and store into array
 # Looping through all the values of dictionary and storing the in an array
 
-for ((i=1;i<=$sizeOfAll_Results;i++))
+for ((i=0;i<$sizeOfAll_Results;i++))
 do
 	Result_Array[i]=${All_Results[result$i]}
 done
 
 #printing the Array values
 echo Array values are : ${Result_Array[@]}
+
+#storing the length of Result_Array
+
+length=${#Result_Array[@]}
+
+# Sorting the array in Descending oreder using Bubble Sort
+for ((i = 0; i<$length; i++)) 
+do
+      
+    for((j = 0; j<$(($length-i-1)); j++)) 
+    do
+      
+        if [ ${Result_Array[j]} -lt ${Result_Array[$((j+1))]} ] 
+        then
+            # swap 
+            temp=${Result_Array[j]} 
+            Result_Array[$j]=${Result_Array[$((j+1))]}   
+            Result_Array[$((j+1))]=$temp 
+        fi
+    done
+done
+  
+echo "Array values in Ascending order is :"
+echo ${Result_Array[@]}
+
+# Sorting the array in Ascending oreder using Bubble Sort
+for ((i = 0; i<$length; i++)) 
+do
+      
+    for((j = 0; j<$(($length-i-1)); j++)) 
+    do
+      
+        if [ ${Result_Array[j]} -gt ${Result_Array[$((j+1))]} ] 
+        then
+            # swap 
+            temp=${Result_Array[j]} 
+            Result_Array[$j]=${Result_Array[$((j+1))]}   
+            Result_Array[$((j+1))]=$temp 
+        fi
+    done
+done
+  
+echo "Array values in Ascending order is :"
+echo ${Result_Array[@]}
