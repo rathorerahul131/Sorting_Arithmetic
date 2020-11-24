7cbd7b3968a6b26186f754971f64ef7276f45849
neelrahulr <rathorerahul131@gmail.com>
2020-11-24 13:14:43 +0530

    Refactored:Stored Computed Results in Dictionary

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -16,7 +16,6 @@ echo "The values of a,b,c are $a, $b, $c"
 result1=$(( $a + $b * $c ))
 echo "The output for the expression a+b*c is $result1"
 
-
 result2=$(( $a * $b + $c ))
 echo "The output for the expression a*b+c is $result2"
 
@@ -25,3 +24,13 @@ echo "The output for the expression c+a/b is $result3"
 
 result4=$(( $a % $b + $c ))
 echo "The output for the expression a%b+c is $result4"
+
+# Created a dictionary named All_Results to store all the results
+declare -A All_Results
+
+for ((i=1;i<5;i++))
+do
+	All_Results["result"$i]=$((result$i))
+done
+
+echo "All the results are ${All_Results[@]}"
